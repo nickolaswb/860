1a08399e768369469bf640b37f3a59fa0265f2dc
nickolaswb <jorgeomaior123@gmail.com>
2026-04-17 15:31:01 -0300

    Add files via upload

---
+++ 860.c
@@ -0,0 +1,37 @@
+bool lemonadeChange(int* bills, int billsSize) {
+    int cinco = 0, dez = 0, vinte = 0;
+
+    for (int i = 0; i<billsSize; i++) {
+        if (bills[i] == 5) {
+            cinco++;
+
+        }
+        else if (bills[i] == 10) {
+            if (cinco > 0) {
+            cinco--;
+            dez++;
+            }
+            else  {
+                return false;
+            }
+        }
+        else if (bills[i] == 20) {
+            if (cinco > 0 && dez > 0) {
+                cinco--;
+                dez--;
+                vinte++;
+
+            }
+            else if (cinco >= 3) {
+                cinco--;
+                cinco--;
+                cinco--;
+                vinte++;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
